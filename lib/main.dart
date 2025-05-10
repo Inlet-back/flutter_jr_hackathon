657e9b2544ca3e99ba265720b6051ee51dd9262e
Masachika Ura <132348270+Inlet-back@users.noreply.github.com>
2025-05-11 04:08:47 +0900

    Revert "クリスタルテーマと中華風テーマを作成"

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,37 @@
+import 'package:alarm/alarm.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_jr_hackathon/route/router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 画面を縦向きに固定する
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  // 確認ログ
+  // setupLogging(showDebugLogs: true);
+
+  await Alarm.init();
+
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-    // テーマ変更時に再描画を強制
-    AppRouter.themeManager.addListener(() {
-      setState(() {});
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      theme: AppRouter.themeManager.currentTheme, // 現在のテーマを適用
-      routerConfig: AppRouter.router, // GoRouterを設定
+      // theme: ThemeData(
+      //   fontFamily: 'CustomFont', // アプリ全体にカスタムフォントを適用
+      // ),
+      routerConfig: AppRouter.router,
     );
   }
 }
--- lib/route/router.dart
+++ lib/route/router.dart
@@ -1,6 +1,5 @@
 import 'package:alarm/model/alarm_settings.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_jr_hackathon/common/themes/theme_manager.dart';
 import 'package:flutter_jr_hackathon/scenes/check/check_screen.dart';
 import 'package:flutter_jr_hackathon/scenes/clear/clear_screen.dart';
 import 'package:flutter_jr_hackathon/scenes/game/game_screen.dart';
@@ -9,14 +8,11 @@ import 'package:alarm/alarm.dart';
 import 'package:flutter_jr_hackathon/scenes/alarm/alarm_screen.dart';
 
 class AppRouter {
-  static final ThemeManager themeManager = ThemeManager();
   static final GoRouter router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
-        builder: (context, state) => AlarmScreen(
-          themeManager: themeManager,
-        ),
+        builder: (context, state) => AlarmScreen(),
       ),
       GoRoute(
         path: '/check',
@@ -27,9 +23,7 @@ class AppRouter {
         builder: (context, state) {
           final extra = state.extra as Map<String, dynamic>?; // `extra` を取得
           final checkTime = extra?['checkTime'] ?? 0; // デフォルト値を設定
-          return GameScreen(
-            themeManager: themeManager,
-          );
+          return GameScreen();
         },
       ),
       GoRoute(
