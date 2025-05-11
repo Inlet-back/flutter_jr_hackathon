1bc5ae348fe25b9ccda206bccab9195be5c1a973
tanakacha <kntasihc20.18@gmail.com>
2025-05-11 09:42:03 +0900

    fix: テーマが選択できる状態でタイマーを設定 #26

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
+import 'package:alarm/alarm.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_jr_hackathon/route/router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 画面を縦向きに固定する
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  
+  // 確認ログ
+  // setupLogging(showDebugLogs: true);
+
+  // アラームの初期化
+  await Alarm.init();
+
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
